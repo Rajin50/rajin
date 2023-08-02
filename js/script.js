62c25757e581eb9f4b533ea14ed3af0c290d147a
Rajin Ibna Rajuanur Rahman <rajinrahman50@gmail.com>
2023-08-02 18:24:57 +0600

    Rajin's Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings:["", "Undergrade Student", "Web Designer", "Web Developer", "Graphics Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true 
+})
