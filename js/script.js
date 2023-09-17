3438cafbcbfa3d8cef298ae4ebff5386695bcf5d
Rajin Ibna Rajuanur Rahman <rajinrahman50@gmail.com>
2023-09-18 01:09:48 +0600

    script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Typing Animation */
 var typed = new Typed(".typing", {
-    strings:["", "Undergrade Student", "Web Designer", "Web Developer", "Graphics Designer"],
+    strings:["", "Undergrad Student", "Programmer", "Web Developer", "Web Designer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true 
